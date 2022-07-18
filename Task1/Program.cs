c5dcaef13db70cc80e347d8b3ba3440280219df9
Tasa <greatwillow@mail.ru>
2022-07-18 15:40:03 +0300

    first task

---
+++ Task1/Program.cs
@@ -0,0 +1,39 @@
+﻿int[] FillArray(int count)
+{
+    int[] array = new int[count];
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
+
+int CountPositiveNumber(int[] array)
+{
+    int count = 0;
+    
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i]>0)
+            count++;
+    }
+
+    return count;
+}
+
+
+Console.WriteLine("Сколько чисел вы хотите ввести?");
+int number = int.Parse(Console.ReadLine());
+int[] array = FillArray(number);
+PrintArray(array);
+Console.WriteLine();
+int result = CountPositiveNumber(array);
+Console.WriteLine(result);
