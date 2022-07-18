a395fca57856f0d03c3cd73151fcdad2f6e310dd
Tasa <greatwillow@mail.ru>
2022-07-18 16:25:08 +0300

    second task

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿void FindCrossDot (float b1, float k1, float b2, float k2)
+{
+    float x = 0;
+    float y = 0;
+    if (k1==k2)
+        Console.WriteLine("прямые параллельны");
+    else {
+        x = (b2-b1)/(k1-k2);
+        y = (k1*b2-k2*b1)/(k1-k2);
+        }
+   Console.WriteLine($"Координаты точки пересечения {x}, {y}");
+}
+Console.WriteLine("введите коэффициент уравнения прямой b1");
+float b1 = float.Parse(Console.ReadLine());
+Console.WriteLine("введите коэффициент уравнения прямой k1");
+float k1 = float.Parse(Console.ReadLine());
+Console.WriteLine("введите коэффициент уравнения прямой b2");
+float b2 = float.Parse(Console.ReadLine());
+Console.WriteLine("введите коэффициент уравнения прямой k2");
+float k2 = float.Parse(Console.ReadLine());
+
+FindCrossDot (b1, k1, b2, k2);
